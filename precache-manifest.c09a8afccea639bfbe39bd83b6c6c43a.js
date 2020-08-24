ebbd61fa158b25945d3d58185456ede3eca533d0
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-08-24 16:55:54 +0530

    Updates

---
+++ precache-manifest.c09a8afccea639bfbe39bd83b6c6c43a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126217a6eca6093a98fa3ac8fc66e2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4fedc11af90ce2253ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f58ab5536a9f524024e9",
+    "url": "/static/js/2.26610747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26610747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fedc11af90ce2253ad",
+    "url": "/static/js/main.081ec084.chunk.js"
+  },
+  {
+    "revision": "c5484114cf3ec8719efd",
+    "url": "/static/js/runtime-main.b92a1f40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
