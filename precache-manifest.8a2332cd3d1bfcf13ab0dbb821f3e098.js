6bde122fc47945c84743543e22653dbb155a8c7d
Pankaj Vaghela <vpankaj1998official@gmail.com>
2021-05-31 19:08:22 +0530

    Updates

---
+++ precache-manifest.8a2332cd3d1bfcf13ab0dbb821f3e098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02be3eeee7475caf6f957ae7eb9fbbd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21db73f6d4e0bdbe5ac7",
+    "url": "/static/css/main.c95b650b.chunk.css"
+  },
+  {
+    "revision": "837554b4a7a29ff4b61b",
+    "url": "/static/js/2.5e88f2e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5e88f2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21db73f6d4e0bdbe5ac7",
+    "url": "/static/js/main.731e5557.chunk.js"
+  },
+  {
+    "revision": "c5484114cf3ec8719efd",
+    "url": "/static/js/runtime-main.b92a1f40.js"
+  },
+  {
+    "revision": "8b4cae31bfd4eb4f42618a1f87e356da",
+    "url": "/static/media/pankaj-vaghela-white-shirt-clipped.8b4cae31.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e222e2f5dc97d87dd762617edf843513.js"
+  "/precache-manifest.8a2332cd3d1bfcf13ab0dbb821f3e098.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
